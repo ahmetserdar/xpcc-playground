dbc8befa2e4bc79de38e63e75a61b12126e7c437
Electronic Kiwi <electron.kiwi@gmail.com>
2014-05-18 01:24:30 +0200

    Start nrf24l01 test

---
+++ nrf24l01/stm32f4/main.cpp
@@ -0,0 +1,27 @@
+// coding: utf-8
+/* Copyright (c) 2014, Electronic Kiwi, daniel-k
+* All Rights Reserved.
+*
+* The file is part of the xpcc-playground and is released under the 3-clause BSD
+* license. See the file `LICENSE` for the full license governing this code.
+*/
+
+#include <xpcc/architecture.hpp>
+#include "../../xpcc/examples/stm32f4_discovery/stm32f4_discovery.hpp"
+
+
+typedef LedBlue Led;
+
+MAIN_FUNCTION
+{
+	defaultSystemClock::enable();
+
+	Led::setOutput(xpcc::Gpio::Low);
+
+
+	while (1)
+	{
+		Led::toggle();
+		xpcc::delay_ms(500);
+	}
+}
